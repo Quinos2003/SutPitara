11c0f48ccb4629beb01f0c98a0b879fc8d57303f
amansoni221010 <113816865+amansoni221010@users.noreply.github.com>
2023-04-03 22:49:29 +0530

    produtct page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter} from 'react-router-dom'
+import { Box, styled } from "@mui/material";
+import Home from './MyComponents/home/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './MyComponents/header/Header';
 function App() {
   return (
@@ -9,17 +11,17 @@ function App() {
     <BrowserRouter>
     
         <Header/>
-        {/* <Box>
+        <Box>
     
     
           <Routes>
     
             <Route path='/' element={<Home/>}/>
-            <Route path='/product/:id' element={<DetailVeiw/>}/>
-            <Route path='/men' element={<ForMen/>}/>
+            {/* <Route path='/product/:id' element={<DetailVeiw/>}/> */}
+            {/* <Route path='/men' element={<ForMen/>}/> */}
             
             </Routes>
-        </Box> */}
+        </Box>
       
      
      
