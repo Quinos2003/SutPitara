76f35378648d0d3207d1e6db76d9c0e271e52b79
Quinos2003 <asthana.samarth2@gmail.com>
2023-06-21 00:18:24 +0530

    added home.py

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("login", views.login_view, name="login"),
-    path("signup", views.signup, name="signup")
+    path("signup", views.signup, name="signup"),
+    
+
 ]
 
 #http://localhost:8000/store/login
