cd42c8ac9d3ef72bf3bcb3a0c434d3a93a2efd62
Quinos2003 <asthana.samarth2@gmail.com>
2023-07-04 03:23:00 +0530

    UPdated2 Middleware to JWT Tokens implementation

--- store/Middleware/auth.py
+++ store/Middleware/auth.py
@@ -1,21 +1,42 @@
+# from django.shortcuts import redirect
+# from .jwt_utils import decode_jwt_token
+
+# def auth_middleware(get_response):
+#     # One-time configuration and initialization.
+
+#     def middleware(request):
+#         token = request.COOKIES.get('token')
+#         if not token:
+#             return redirect('login')  # Assuming there's a login endpoint defined
+
+#         try:
+#             decoded_token = decode_jwt_token(token)
+#             # Add the decoded token or relevant user information to the request object
+#             request.user = decoded_token['user']
+#         except Exception as e:
+#             # Handle token validation error
+#             return redirect('login')  # Assuming there's a login endpoint defined
+
+#         response = get_response(request)
+#         return response
+
+#     return middleware
+
 from django.shortcuts import redirect
-from .jwt_utils import decode_jwt_token
+from .jwt import decode_jwt_token
 
 def auth_middleware(get_response):
-    # One-time configuration and initialization.
-
     def middleware(request):
         token = request.COOKIES.get('token')
+
         if not token:
-            return redirect('login')  # Assuming there's a login endpoint defined
+            return redirect('login')
 
         try:
             decoded_token = decode_jwt_token(token)
-            # Add the decoded token or relevant user information to the request object
-            request.user = decoded_token['user']
-        except Exception as e:
-            # Handle token validation error
-            return redirect('login')  # Assuming there's a login endpoint defined
+            request.user_id = decoded_token['user_id']
+        except:
+            return redirect('login')
 
         response = get_response(request)
         return response
