5f8ff2dd6ec607692525543f3b943f081da1ec86
Quinos2003 <asthana.samarth2@gmail.com>
2023-07-04 03:00:58 +0530

    UPdated Middleware to JWT Tokens implementation

--- store/Middleware/auth.py
+++ store/Middleware/auth.py
@@ -1,14 +1,21 @@
 from django.shortcuts import redirect
+from .jwt_utils import decode_jwt_token
 
 def auth_middleware(get_response):
     # One-time configuration and initialization.
 
     def middleware(request):
-        print(request.session.get('customer'))
-        returnUrl = request.META['PATH_INFO']
-        print(request.META['PATH_INFO'])
-        if not request.session.get('customer'):
-           return redirect(f'login?return_url={returnUrl}')
+        token = request.COOKIES.get('token')
+        if not token:
+            return redirect('login')  # Assuming there's a login endpoint defined
+
+        try:
+            decoded_token = decode_jwt_token(token)
+            # Add the decoded token or relevant user information to the request object
+            request.user = decoded_token['user']
+        except Exception as e:
+            # Handle token validation error
+            return redirect('login')  # Assuming there's a login endpoint defined
 
         response = get_response(request)
         return response
