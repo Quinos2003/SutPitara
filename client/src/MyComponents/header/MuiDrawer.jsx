8f69a754ecf93141c185ec6a7b94bd4c30e354ed
VishalJx <itsmevishal360@gmail.com>
2023-07-17 22:03:40 +0530

    updated ham menu

--- client/src/MyComponents/header/MuiDrawer.jsx
+++ client/src/MyComponents/header/MuiDrawer.jsx
@@ -1,6 +1,7 @@
 import { Drawer,Box, IconButton, Typography,styled } from '@mui/material'
 import React, { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
+import { Link } from 'react-router-dom'
 
 
 
@@ -24,10 +25,10 @@ const MuiDrawer = () => {
     onClose={()=> setIsDrawerOpen(false)}  >
         <Box sx={{ overflow: 'auto',
       width: '50vw' }}  textAlign='center' role='presentation'>
-            <Typography variant='h6' component='div'>Previous Orders</Typography>
-            <Typography variant='h6' component='div'>My Profile</Typography>
-            <Typography variant='h6' component='div'>Login</Typography>
-            <Typography variant='h6' component='div'>Wishlist</Typography>
+            <Link to='/orders' style={{textDecoration:"none", color:"black"}}><Typography variant='h6' component='div'>Previous Orders</Typography></Link>
+            <Link to='/account' style={{textDecoration:"none", color:"black"}}><Typography variant='h6' component='div'>My Profile</Typography></Link>
+            <Link to='/login' style={{textDecoration:"none", color:"black"}}><Typography variant='h6' component='div'>Login</Typography></Link>
+            <Link to='/wishlist' style={{textDecoration:"none", color:"black"}}><Typography variant='h6' component='div'>Wishlist</Typography></Link>
             
         </Box>
     </Drawer>
